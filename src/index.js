0bf011ed3fbf4205e44110602011bf09b2202621
xenleme <xenleme@gmail.com>
2019-03-20 20:47:42 +0300

    Example of reducer function without using redux for practice of understanding redux architecture

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hi there!');
+// Example of increment function
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+let state = reducer(undefined, {});
+
+state = reducer(state, { type: 'INC' });
+console.log(state);
+
+state = reducer(state, { type: 'INC' });
+console.log(state);
