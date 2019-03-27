d531cb379f8e51808b058f1291071896780c84e1
xenleme <xenleme@gmail.com>
2019-03-27 08:37:26 +0300

    Added ui for redux without using react

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
       return state + 1;
+    case 'DEC':
+      return state - 1;
     default:
       return state;
   }
@@ -11,9 +13,19 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState());
-});
+document.getElementById('inc')
+        .addEventListener('click', () => {
+          store.dispatch({ type: 'INC' });
+        });
 
-store.dispatch({ type: 'INC' });
-store.dispatch({ type: 'INC' });
+document.getElementById('dec')
+        .addEventListener('click', () => {
+          store.dispatch({ type: 'DEC' });
+        });
+
+const update = () => {
+  document.getElementById('counter')
+          .innerHTML = store.getState();
+};
+
+store.subscribe(update);
