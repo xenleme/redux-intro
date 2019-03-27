5e47688ed6e365ba46b0afc06a5733e7a0700fd7
xenleme <xenleme@gmail.com>
2019-03-27 08:46:34 +0300

    Used actions with additional params

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { createStore } from 'redux';
 
 const reducer = (state = 0, action) => {
   switch (action.type) {
+    case 'RND':
+      return state + action.payload;
     case 'INC':
       return state + 1;
     case 'DEC':
@@ -21,6 +23,15 @@ document.getElementById('inc')
 document.getElementById('dec')
         .addEventListener('click', () => {
           store.dispatch({ type: 'DEC' });
+        });
+
+document.getElementById('rnd')
+        .addEventListener('click', () => {
+          const payload = Math.floor(Math.random()*10);
+          store.dispatch({
+            type: 'RND',
+            payload
+          });
         });
 
 const update = () => {
