a25d1f4903665a93721fc72acc79fbbf39a44b04
xenleme <xenleme@gmail.com>
2019-03-27 08:59:51 +0300

    Moved reducer and action creator functions to separate files

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import { createStore } from 'redux';
-
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'RND':
-      return state + action.payload;
-    case 'INC':
-      return state + 1;
-    case 'DEC':
-      return state - 1;
-    default:
-      return state;
-  }
-};
+import reducer from './reducer';
+import { inc, dec, rnd } from './actions';
 
 const store = createStore(reducer);
-
-const inc = () => ({ type: 'INC' });
-const dec = () => ({ type: 'DEC' });
-const rnd = (payload) => ({ type: 'RND', payload });
 
 document.getElementById('inc')
         .addEventListener('click', () => {
