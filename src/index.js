81cf4206931016dea8aa77fb94e483eef3409d55
xenleme <xenleme@gmail.com>
2019-03-27 09:36:33 +0300

    Using react with redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './counter';
+
 import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
+
 import * as actions from './actions';
 
 const store = createStore(reducer);
@@ -8,21 +13,19 @@ const { dispatch } = store;
 
 const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-document.getElementById('inc')
-        .addEventListener('click', inc);
-
-document.getElementById('dec')
-        .addEventListener('click', dec);
-
-document.getElementById('rnd')
-        .addEventListener('click', () => {
-          const payload = Math.floor(Math.random()*10);
-          rnd(payload);
-        });
-
 const update = () => {
-  document.getElementById('counter')
-          .innerHTML = store.getState();
+  ReactDOM.render(
+    <Counter 
+      counter={store.getState()}
+      inc={inc}
+      dec={dec}
+      rnd={() => {
+        const value = Math.floor(Math.random()*10);
+        rnd(value);
+      }} />, 
+    document.getElementById('root')
+  );
 };
 
+update();
 store.subscribe(update);
